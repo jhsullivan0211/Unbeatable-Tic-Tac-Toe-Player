a68b9be7de11345444361689b91350daffb20763
Joshua Sullivan <joshuasullivan0211@gmail.com>
2018-06-28 13:55:48 -0500

    Added check for 3 in a row, fixed up some code.
    Added a data structure to GameBoard that tracks the number of Xs and Os in each column, row, and diagonal, as well as a method to test whether any player has three in a row.

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TicTacToe;
 
 namespace TicTacToeEngine
 {
     public partial class MainForm : Form
     {
+        TicTacToe.InputReceiver receiver;
+
         public MainForm()
         {
             InitializeComponent();
@@ -19,9 +22,16 @@ namespace TicTacToeEngine
 
         private void BoardPicture_Click(object sender, EventArgs e)
         {
-           
+            MouseEventArgs click = (MouseEventArgs) e;
+
+            if (receiver == null) return;
+            receiver.ProcessClick(click.Location.X, click.Location.Y);
         }
 
+        public void SetInputReceiver(InputReceiver receiver)
+        {
+            this.receiver = receiver;
+        }
         
     }
 }
--- WindowsFormsApp2/Program.cs
+++ WindowsFormsApp2/Program.cs
@@ -11,7 +11,7 @@ namespace TicTacToe
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application.  Creates the necessary game data and loads the form.
         /// </summary>
         [STAThread]
         static void Main()
@@ -20,52 +20,142 @@ namespace TicTacToe
             Application.SetCompatibleTextRenderingDefault(false);
 
             MainForm form = new MainForm();
-            GameBoard gameBoard = new GameBoard();
-            gameBoard.Mark(2, 1);
-            gameBoard.Mark(0, 0);
-            BoardDrawer test = new BoardDrawer(gameBoard, form);
-            test.Update();
+            GameBoard gameBoard = new GameBoard();          
+            BoardDrawer drawer = new BoardDrawer(gameBoard, form);
+            InputReceiver inputReceiver = new InputReceiver(gameBoard, drawer);
+            form.SetInputReceiver(inputReceiver);
 
             Application.Run(form);
         }
     }
 
-    class GameBoard
+    /// <summary>
+    /// A class to receive click information from MainForm forms and mark the board if 
+    /// the click is in the right place.
+    /// </summary>
+    public class InputReceiver
+    {
+        GameBoard board;
+        PictureBox boardPicture;
+        BoardDrawer drawer;
+
+        /// <summary>
+        /// Basic constructor for the InputReceiver class.
+        /// </summary>
+        /// <param name="board">The GameBoard to mark where clicked, if in a valid position.</param>    
+        /// <param name="boardPicture">The PictureBox that displays the board.</param>
+        /// <param name="drawer">The BoardDrawer </param>
+        public InputReceiver(GameBoard board, BoardDrawer drawer)
+        {
+            this.board = board;
+            this.boardPicture = drawer.BoardPicture;
+            this.drawer = drawer;
+        }
+
+        /// <summary>
+        /// Receives click information from the form and acts on the GameBoard, marking the appropriate
+        /// place.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void ProcessClick(int x, int y)
+        {
+            int unitX = boardPicture.Width / 3;
+            int unitY = boardPicture.Height / 3;
+
+            int convertedX = x / unitX;
+            int convertedY = y / unitY;
+
+            if (board.Mark(convertedX, convertedY))
+            {
+                char winner = board.TestBoard();
+                if (winner != 'N')
+                {
+                    Debug.WriteLine(winner);
+                }
+                drawer.Update();
+            }
+
+        }
+    }
+
+
+    public class GameBoard
     {
         char[,] moveMatrix;
         char currentTurn = 'X';
         int turnNumber = 0;
+        int[] lastMove;
+                                       //X, O
+        int[][] rowMarks = { new int[] { 0, 0 },        //row 0
+                             new int[] { 0, 0 },        //row 1
+                             new int[] { 0, 0 } };      //row 2
 
-        public GameBoard(char[,] moveMatrix = null)
+        int[][] columnMarks = { new int[] { 0, 0 },     //col 0
+                                new int[] { 0, 0 },     //col 1
+                                new int[] { 0, 0 } };   //col 2
+
+        int[][] diagonalMarks = { new int[] { 0, 0 },   //up-left diag
+                                  new int[] { 0, 0 }};  //down-right diag
+
+
+        public GameBoard()
         {
-            if (moveMatrix == null)
+            moveMatrix = GetEmptyMatrix();
+        }
+
+        public bool Mark(int x, int y)
+        {
+            if (moveMatrix[y, x] != 'N')
             {
-                this.moveMatrix = GetEmptyMatrix();
+                return false;
             }
-           else
-           {
-                this.moveMatrix = moveMatrix;
-                int count = 0;
-                foreach (char mark in moveMatrix)
+
+            moveMatrix[y, x] = currentTurn;
+            int markIndex = (currentTurn == 'X') ? 0 : 1;
+            turnNumber++;
+            rowMarks[y][markIndex]++;
+            columnMarks[x][markIndex]++;
+
+            if (y - x == 0)
+            {
+                diagonalMarks[0][markIndex]++;
+            }
+            if (y - x == 2 || y - x == -2 || (y - x == 0 && y == 1))
+            {
+                diagonalMarks[1][markIndex]++;
+            }
+
+            currentTurn = (currentTurn == 'X') ? 'O' : 'X';
+            lastMove = new int[] { x, y };
+            return true;
+        }
+
+        public char TestBoard()
+        {
+            int[][][] groups = { rowMarks, columnMarks, diagonalMarks };
+            foreach (int[][] group in groups)
+            {
+                for (int i = 0; i < group.Length; i++)
                 {
-                    if (mark != 'N')
+                    if (group[i][0] == 3)
                     {
-                        count += 1;
+                        return 'X';
+                    }
+                    if (group[i][1] == 3)
+                    {
+                        return 'O';
                     }
                 }
-                turnNumber = count;
-           }
-        }
-
-        public char[,] MoveMatrix
-        {
-            get { return this.moveMatrix; }
-        }
-
-        public void Mark(int x, int y)
-        {
-            moveMatrix[y, x] = currentTurn;
-            currentTurn = (currentTurn == 'X') ? 'O' : 'X';
+            }
+            if (turnNumber == 9)
+            {
+                return 'T';
+            }
+            else
+            {
+                return 'N';
+            }
         }
 
         public static char[,] GetEmptyMatrix()
@@ -75,53 +165,57 @@ namespace TicTacToe
                                  { 'N', 'N', 'N' } };
         }
 
-        public char[,] GetMoveMatrix()
+
+        public char[,] MoveMatrix
         {
-            return this.moveMatrix;
+            get { return this.moveMatrix; }
         }
 
-        public void Print()
+        public int[] LastMove
         {
-            foreach (char c in this.moveMatrix)
-            {
-                Debug.WriteLine(c);
-            }
+            get { return this.lastMove; }
         }
+
+        public char CurrentTurn
+        {
+            get { return this.currentTurn; }
+        }
+
     }
 
-    class BoardDrawer
+
+    public class BoardDrawer
     {
         GameBoard gameBoard;
-        PictureBox pictureBox;
-        Stack<PictureBox> xMarkPool = new Stack<PictureBox>();
-        Stack<PictureBox> oMarkPool = new Stack<PictureBox>();
+        PictureBox boardPicture;
         MainForm form;
-
 
         public BoardDrawer(GameBoard gameBoard, MainForm form)
         {
             this.gameBoard = gameBoard;
             this.form = form;
 
-            this.pictureBox = form.BoardPicture;
-
-            for (int i = 0; i < 4; i++)
-            {
-                xMarkPool.Push(CreateMark('X'));
-                oMarkPool.Push(CreateMark('O'));     
-            }
-            xMarkPool.Push(CreateMark('X'));
+            this.boardPicture = form.BoardPicture;
+            boardPicture.SendToBack();
+       
         }
 
-        private PictureBox CreateMark(char marking)
+        public PictureBox BoardPicture
+        {
+            get { return this.boardPicture; }
+        }
+
+        private PictureBox CreateMark(char marking, int x, int y)
         {
             PictureBox mark = new PictureBox();
             mark.SizeMode = PictureBoxSizeMode.StretchImage;
             mark.Width = 100;
             mark.Height = 100;
-            mark.Parent = pictureBox;
+            mark.Parent = boardPicture;
             mark.BackColor = System.Drawing.Color.Transparent;
-            mark.Visible = false;
+            mark.Left = x;
+            mark.Top = y;
+
             if (marking == 'O')
             {
                 mark.Image = TicTacToeEngine.Properties.Resources.TicTacToeO;
@@ -131,54 +225,27 @@ namespace TicTacToe
                 mark.Image = TicTacToeEngine.Properties.Resources.TicTacToeX;
             }
 
+
             form.Controls.Add(mark);
             return mark;
         }
 
         public void Update()
         {
-            pictureBox.SendToBack();
-            for (int i = 0; i < gameBoard.MoveMatrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < gameBoard.MoveMatrix.GetLength(1); j++)
-                {
-                    char mark = gameBoard.MoveMatrix[i, j];
-                    PictureBox markImage = null;
-                    switch (mark)
-                    {
-                        case 'X':
-                            markImage = xMarkPool.Pop();
-                            break;
-                        case 'O':
-                            markImage = oMarkPool.Pop();
-                            break;
-                        case 'N':
-                            continue;
-                    }
-
-                    
-                    int[] point = { j, i };
-                    int[] convertedPoint = GetScreenPoint(point);
-                    Debug.WriteLine(pictureBox.Left);
-                    markImage.Left = pictureBox.Left + convertedPoint[0];
-                    markImage.Top = pictureBox.Top + convertedPoint[1];
-                    markImage.Visible = true;
-                }
-            }
+            char mark = gameBoard.CurrentTurn == 'X' ? 'O' : 'X';
+            int[] point = GetScreenPoint(gameBoard.LastMove);
+            PictureBox markPicture = CreateMark(mark, point[0], point[1]);
+            markPicture.Parent = boardPicture;
         }
 
         private int[] GetScreenPoint(int[] point)
         {
-            int unit = Math.Min(this.pictureBox.Width / 3, this.pictureBox.Height / 3);
+            int unit = Math.Min(this.boardPicture.Width / 3, this.boardPicture.Height / 3);
             int offset = 40;
-
             int[] result = new int[2];
-            result[0] = point[0] * unit + offset;
-            result[1] = point[1] * unit + offset;
+            result[0] = (point[0] * unit + offset);
+            result[1] = (point[1] * unit + offset);
             return result;
         }
     }
-
-   
-
 }
