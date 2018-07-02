2d44732300ff4a22c6c26723524e0781cdde74dc
Joshua Sullivan <joshuasullivan0211@gmail.com>
2018-07-01 19:20:45 -0500

    Changed some project settings.
    Renamed the project in the application settings.

--- WindowsFormsApp2/Properties/AssemblyInfo.cs
+++ WindowsFormsApp2/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApp2")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TicTacToeEngine")]
+[assembly: AssemblyDescription("A Tic-Tac-Toe playing AI.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp2")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("jhsullivan0211")]
+[assembly: AssemblyProduct("TicTacToeEngine")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
